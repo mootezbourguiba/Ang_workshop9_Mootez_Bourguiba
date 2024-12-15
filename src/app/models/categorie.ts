479ea00fbacaa60a552b92a544a86d4d9bbe9147
Mootez Bourguiba <mootezbourguiba73@gmail.com>
2024-12-15 14:16:56 +0100

    Suppression de tout le contenu actuel

--- src/app/models/categorie.ts
+++
@@ -1,9 +0,0 @@
-export class Categorie {
-  constructor(
-    public id: number,
-    public title: string,
-    public image: string,
-    public description: string,
-    public available: boolean
-  ) {}
-}
